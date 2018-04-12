cded8124767db636edd72bb38f962fd1fe797e3c
ben-tai <jamin.tairea@gmail.com>
2018-04-13 10:12:52 +1200

    home page and button not formatted cool

--- client/App.jsx
+++ client/App.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
+import { HashRouter as Router, Route, Link} from 'react-router-dom'
+
+import Home from './Home'
+import Birds from './Birds'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      birds: [
-        {id: 1, name: 'Kokako', description: 'Brave'},
-        {id: 2, name: 'Kea', description: 'Cheeky'},
-        {id: 3, name: 'Kiwi', description: 'Shy'}
-      ]
-    }
+    // this.state = {
+    //   birds: [
+    //     {id: 1, name: 'Kokako', description: 'Brave'},
+    //     {id: 2, name: 'Kea', description: 'Cheeky'},
+    //     {id: 3, name: 'Kiwi', description: 'Shy'}
+    //   ]
+    // }
    
   }
 
   render() {
-    console.log(this.state)
-    return <div>
-      <h1>Birds</h1>
-      <hr />
-      <p>Here they are</p>
-    </div>
+    return (
+      <Router>
+        <div>
+          <Route path = '/' component = {Home} />
+          <Route path = '/birds' component = {Birds} />
+        </div>
+      </Router>
+    )  
   }
 }
 
