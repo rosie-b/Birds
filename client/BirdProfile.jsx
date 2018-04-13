38fa3c66ead0c64ae978b8f41ed35e68120213e6
Annika Philipp <sprotty01@gmail.com>
2018-04-13 14:45:27 +1200

    hover effect initiated

--- client/BirdProfile.jsx
+++ client/BirdProfile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import birds from '../public/data/birds'
 
-const BirdProfile = (props) => {
+const BirdProfile = (props) =>  {
 
 	console.log(props)
 	const name = props.bird.match.params.bird 
@@ -13,7 +13,11 @@ const BirdProfile = (props) => {
        }
 
     })
-    // console.log(findBird)
+console.log(findBird.sound)
+    // const sound = findBird.sound
+    // const playSound = () =>{
+    //     sound.play();
+    // }
     return (
         <React.Fragment>
             <div className="overbody">
@@ -23,12 +27,13 @@ const BirdProfile = (props) => {
 					</div>
 					<div className="birdprofile">
 						<div className='upperContainer'>
-                        <div className="birdimg">
+                        <div className="birdimg" id="hvr-grow">
                            
-                            <audio id="myAudio">
-                                <source src="{findBird.sound}" type="audio/mpeg" />
-                            </audio>
-							<img className="birdpicture" src={findBird.image_url} alt="birdpic"/>
+                            {/* <audio id="myAudio">
+                                <source src={findBird.sound} type="audio/mpeg"/>
+                            </audio> */}
+
+							<img className="birdpicture" src={findBird.image_url} alt="birdpic" />
                         
                       
                         </div>
@@ -45,7 +50,7 @@ const BirdProfile = (props) => {
                       
                         <div className="birdDescription">
                             <p>{findBird.description}</p>
-                            <p>{findBird.status}</p>
+                            <p>Status: {findBird.status}</p>
                         </div>
                     </div>
 				</div>		
