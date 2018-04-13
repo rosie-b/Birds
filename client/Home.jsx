73aa63d95cbda2f1c6739bb5c94e8b0fdc89a4c7
ben-tai <jamin.tairea@gmail.com>
2018-04-13 12:32:17 +1200

    formatted home page

--- client/Home.jsx
+++ client/Home.jsx
@@ -6,7 +6,8 @@ const Home = () => {
   return (
     <div>
         <h2 className="welcome">Haere Mai! Welcome friends!</h2>
-        <h1 className="title">Birds of EDA</h1>
+        <h1 className="title">Birds</h1>
+        <h1 className="title">of EDA</h1>
         <Link to = {'/birds'}><button type = "button">See them birds innit</button></Link>
     </div>
   )    
